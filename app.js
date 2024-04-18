5bb224de8abdfd4945caae081a2c751b5d6cf29d
syedauraain <160678874+syedauraain@users.noreply.github.com>
2024-04-18 15:24:36 -0700

    Did Stripping_Names

---
+++ app.js
@@ -0,0 +1,5 @@
+"use strict";
+const personname = '   \n\tSYED AURAAIN YUSHA ZAIDI\t\n   ';
+console.log(personname);
+const without_whitespace = personname.trim();
+console.log(without_whitespace);
---
+++ app.ts
@@ -0,0 +1,7 @@
+const  personname :string = '   \n\tSYED AURAAIN YUSHA ZAIDI\t\n   ' ;
+
+console.log(personname);
+
+const without_whitespace :string = personname.trim();
+
+console.log(without_whitespace);
